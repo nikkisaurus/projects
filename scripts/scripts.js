66b838f3a5cdd4fda1c77316a9edcde95ae0b85a
Jasmine Vellocido <jlvellocido@gmail.com>
2020-04-09 17:46:34 -0500

    Basic site completed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
     if ((/#projects$/).test(document.location) === true) {
         $("main").load("projects/index.html")
+    } else if ((/#contact$/).test(document.location) === true) {
+        $("main").load("contact.html")
     } else {
-        $("main").load("main.html")
+        // $("main").load("main.html")
+        $("main").load("projects/index.html")
     }
 
     $("header a").on("click", function () {
-        $("main").load("main.html")
+        // $("main").load("main.html")
+        $("main").load("projects/index.html")
     })
 
     $("nav a").on("click", function (slug) {
