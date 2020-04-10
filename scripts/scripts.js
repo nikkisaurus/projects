d6f98b35e55489e81414c9cacc5026c9851b2d0c
Jasmine Vellocido <jlvellocido@gmail.com>
2020-04-09 23:18:08 -0500

    Revamped layout

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,23 +1,6 @@
 $(document).ready(function () {
-    if ((/#portfolio$/).test(document.location) === true) {
-        $("main").load("art/portfolio.html")
-    } else if ((/#graphics$/).test(document.location) === true) {
-        $("main").load("art/index.html")
-    } else if ((/#projects$/).test(document.location) === true) {
-        $("main").load("projects/index.html")
-    } else if ((/#contact$/).test(document.location) === true) {
-        $("main").load("contact.html")
-    } else {
-        $("main").load("main.html")
-        // $("main").load("projects/index.html")
-    }
-
-    $("header a").on("click", function () {
-        $("main").load("main.html")
-        // $("main").load("projects/index.html")
-    })
-
-    $("nav a").on("click", function (slug) {
-        $('main').load(slug.target.id)
-    })
+    $("main").load("main.html")
+    $("#projectsContainer").load("projects/index.html")
+    $("#graphicsContainer").load("art/graphics.html")
+    $("#artContainer").load("art/art.html")
 });
