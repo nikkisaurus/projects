c8549dae05664336a6f2c3bbb04227dc12c8bc13
Jasmine Vellocido <jlvellocido@gmail.com>
2020-04-14 02:10:58 -0500

    Updated portfolio layout

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,6 +1,2 @@
 $(document).ready(function () {
-    $("main").load("main.html")
-    $("#projectsContainer").load("projects/index.html")
-    $("#graphicsContainer").load("art/graphics.html")
-    $("#artContainer").load("art/art.html")
 });
