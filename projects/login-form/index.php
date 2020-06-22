58523b25bad2fd6b1fa5a6bff020fcc06f4e7707
Jasmine Vellocido <jlvellocido@gmail.com>
2020-06-22 14:14:06 -0500

    Database connection for login_form

---
+++ projects/login-form/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <?php
+    // Include config file
+    require_once "assets/config.php";
+
+    echo DB_PASSWORD;
+?>
+
+</body>
+</html>
