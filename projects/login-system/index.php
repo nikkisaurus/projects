c6fbe36c5b5a0481ac4375cc5ded7794277b4e94
Jasmine Vellocido <jlvellocido@gmail.com>
2020-06-22 18:15:01 -0500

    Updated htaccess and gitignore

--- projects/login-system/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Connect to database
-require_once "assets/config.php";
-require_once "controllers/database.php";
-$db = new Database(sprintf("mysql:host=%s;dbname=%s;port=%u;charset=%s", DB_SERVER, DB_NAME, DB_PORT, DB_CHARSET), DB_USERNAME, DB_PASSWORD);
-
-// Display index
-require_once "views/index.html";
